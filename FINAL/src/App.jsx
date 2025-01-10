efb3d79d739f5905f0229e7dba11fba5ce8d6157
Dristi <103329082+Dris-ti@users.noreply.github.com>
2025-01-10 23:12:14 +0600

    Add files via upload

---
+++ FINAL/src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import LoginPage from './components/Login'
+import Registration from './components/Registration';
+
+function App() {
+  return (
+    <>
+    <Registration/>
+    </>
+  )
+}
+
+export default App
